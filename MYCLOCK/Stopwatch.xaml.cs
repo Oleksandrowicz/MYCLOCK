c81b58039d3500d22be3c39a263659b7afd877c6
Cupr_ms57@student.itstep.org <Cupr_ms57@student.itstep.org>
2023-05-03 17:01:34 +0300

    Stopwatch fix

--- MYCLOCK/Stopwatch.xaml.cs
+++ MYCLOCK/Stopwatch.xaml.cs
@@ -16,8 +16,11 @@ namespace MYCLOCK
         DispatcherTimer time;
         DispatcherTimer dt;
         System.Diagnostics.Stopwatch sw;
+        System.Diagnostics.Stopwatch swlap;
         string currentTime = string.Empty;
         byte clicktimes=0;
+        int clicktimeslap=-1;
+
         public Stopwatch()
         {
             //dispatcherTimer = new DispatcherTimer();
@@ -29,6 +32,15 @@ namespace MYCLOCK
         {
             time = new DispatcherTimer();
             clicktimes++;
+            clicktimeslap++;
+            if ((clicktimeslap % 2) == 0)
+            {
+                lapbtn.Content = "Lap";
+            }
+            else if ((clicktimeslap % 2) != 0)
+            {
+                lapbtn.Content = "Reset";
+            }
             if ((clicktimes % 2) != 0)
             {
                 time.Start();
@@ -37,11 +49,12 @@ namespace MYCLOCK
                 sw = new System.Diagnostics.Stopwatch();
                 sw.Start();
                 dt.Start();
+                
                 startbtn.Background = (Brush)new BrushConverter().ConvertFrom("#2f100d"); 
                 startbtn.Foreground = (Brush)new BrushConverter().ConvertFrom("#e0544b"); 
                 startbtn.Content = "Stop";
                 dt.Interval = new TimeSpan(0, 0, 0, 0, 1);
-
+               
                 //stopWatch.Start();
                 dt.Tick += Dt_Tick;
             }
@@ -62,8 +75,9 @@ namespace MYCLOCK
         private void Dt_Tick(object? sender, EventArgs e)
         {
             
-            if (sw.IsRunning)
+            if (dt.IsEnabled)
             {
+               dt.ToString();
                 TimeSpan ts = sw.Elapsed;
                 currentTime = String.Format("{0:00}:{1:00},{2:00}",
                 ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
@@ -73,19 +87,34 @@ namespace MYCLOCK
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            if ((clicktimes % 2) != 0)
+            try
             {
-                lapbtn.Content = "Lap";
-                time.Stop();
-                lapbox.Items.Add(currentTime);
-                time.Start();
-            }
-            else if ((clicktimes % 2) == 0)
-            {
-                lapbtn.Content = "Reset";
+                //time.Stop();
+               // time.Start();
                 sw.Reset();
-                timelb.Content = "00:00,00";
+                if ((clicktimeslap % 2) == 0)
+                {
+                    //var elapsed = swlap.Elapsed;
+                    //swlap.Restart();
+                    //lapbtn.Content = "Lap";
+                    lapbox.Items.Add(currentTime);
+                    
+                    //time.
+                }
+                else if ((clicktimeslap % 2) != 0)
+                {
+                    //lapbtn.Content = "Reset";
+                    sw.Reset();
+                    timelb.Content = "00:00,00";
+                    lapbox.Items.Clear();
+                }
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                
+            }
+            
 
 
 
