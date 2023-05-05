9d473dea25458705a3cfe00767a8026dbeef5c70
Dimi_oh47@student.itstep.org <Dimi_oh47@student.itstep.org>
2023-05-05 16:29:30 +0300

    Add DataBase

---
+++ Data_access/Entities/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_access.Entities
+{
+    internal class Note
+    {
+        public int ID { get; set; }
+        public string MessageNote { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Data_access/NotesDBContext.cs
@@ -0,0 +1,29 @@
+﻿using Data_access.Entities;
+using Data_access.Helpers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_access
+{
+    internal class NotesDBContext : DbContext
+    {
+        public DbSet <Note> Notes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"workstation id=NotesDataB.mssql.somee.com;packet size=4096;user id=Oleksandrowicz_SQLLogin_1;pwd=raz9464klt;data source=NotesDataB.mssql.somee.com;persist security info=False;initial catalog=NotesDataB");
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Note>().Property(n => n.MessageNote).HasMaxLength(200).IsRequired();
+            modelBuilder.SeedNotes();
+            
+        }
+    }
+}
